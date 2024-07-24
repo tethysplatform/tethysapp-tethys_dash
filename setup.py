4b37c1d42ae46dc8905f685baf2265fbcf24ce2e
Corey Krewson <ckrewson@aquaveo.com>
2024-07-24 15:46:14 -0500

    version 0.0.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ resource_files = find_all_resource_files(app_package, TethysAppBase.package_name
 
 setup(
     name=release_package,
-    version="0.0.1",
+    version="0.0.2",
     description="",
     long_description="",
     keywords="",
