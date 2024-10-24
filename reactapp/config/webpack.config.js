0f5cc2ddb8da20b676ee98aa79f805c5a17fed61
romer8 <gio.busrom@gmail.com>
2024-10-24 16:23:15 -0600

    added remote plugin react component

--- reactapp/config/webpack.config.js
+++ reactapp/config/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
 const Dotenv = require("dotenv-webpack");
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
 module.exports = (env, argv) => {
   const dotEnvPath = `./reactapp/config/${argv.mode}.env`;
@@ -26,6 +27,19 @@ module.exports = (env, argv) => {
       new Dotenv({
         path: dotEnvPath,
       }),
+      new ModuleFederationPlugin({
+        name: 'host_react_module',
+        filename: 'remoteEntry.js',
+        remotes: {
+        },
+        shared: {
+          react: {
+            requiredVersion: false,
+            singleton: true,
+            eager: true,
+          },
+    },
+}),
     ],
     module: {
       rules: [
