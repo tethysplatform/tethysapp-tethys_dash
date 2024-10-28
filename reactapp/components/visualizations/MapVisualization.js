e418a0e2f19b9acad3cd8cddc7b6653197bb89fd
romer8 <gio.busrom@gmail.com>
2024-10-27 20:04:43 -0600

    added changes to the app

--- reactapp/components/visualizations/MapVisualization.js
+++ reactapp/components/visualizations/MapVisualization.js
@@ -1,44 +1,43 @@
-import React, {memo} from "react";
-import { Map } from "../../tethys-ol/providers/Map";
-import Layer from "../../tethys-ol/components/layers/Layer";
-import Source from "../../tethys-ol/lib/Source";
-import Layers from "../../tethys-ol/components/layers/Layers";
-import Controls from "../../tethys-ol/components/controls/Controls";
-import { LayersControl } from "../../tethys-ol/components/controls/LayersControl";
-import View from "../../tethys-ol/components/View";
-import { LegendControl } from "tethys-ol/components/controls/Legend/Legend";
+// import React, {memo} from "react";
+// import { Map } from "../../tethys-ol/providers/Map";
+// import Layer from "../../tethys-ol/components/layers/Layer";
+// import Source from "../../tethys-ol/lib/Source";
+// import Layers from "../../tethys-ol/components/layers/Layers";
+// import Controls from "../../tethys-ol/components/controls/Controls";
+// import { LayersControl } from "../../tethys-ol/components/controls/LayersControl";
+// import View from "../../tethys-ol/components/View";
+// import { LegendControl } from "tethys-ol/components/controls/Legend/Legend";
 
-const MapVisualization = ({ mapConfig, viewConfig,layers,legend }) => {
+// const MapVisualization = ({ mapConfig, viewConfig,layers,legend }) => {
   
-  return (
+//   return (
 
-    <Map {...mapConfig} >
-        <View {...viewConfig} />
-        <Layers>
-        {layers &&
-          layers.map((config, index) => {
-            const {
-              type: LayerType,
-              props: {
-                source: { type: SourceType, props: sourceProps },
-                ...layerProps
-              },
-            } = config;
+//     <Map {...mapConfig} >
+//         <View {...viewConfig} />
+//         <Layers>
+//         {layers &&
+//           layers.map((config, index) => {
+//             const {
+//               type: LayerType,
+//               props: {
+//                 source: { type: SourceType, props: sourceProps },
+//                 ...layerProps
+//               },
+//             } = config;
 
-            const source = Source({ is: SourceType, ...sourceProps });
+//             const source = Source({ is: SourceType, ...sourceProps });
 
-            return (
-              <Layer key={index} is={LayerType} source={source} {...layerProps} />
-            );
-          })}
-        </Layers>
-        <Controls>
-            <LayersControl />
-            <LegendControl items={legend} />
-        </Controls>
+//             return (
+//               <Layer key={index} is={LayerType} source={source} {...layerProps} />
+//             );
+//           })}
+//         </Layers>
+//         <Controls>
+//             <LayersControl />
+//             <LegendControl items={legend} />
+//         </Controls>
+//     </Map>
+//   );
+// }
 
-    </Map>
-  );
-}
-
-export default memo(MapVisualization)
+// export default memo(MapVisualization)
