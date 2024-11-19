6ca9706eedbefdbf14dd3716cf6d66fa5ace1777
Corey Krewson <ckrewson@aquaveo.com>
2024-11-19 09:49:40 -0700

    LayoutAlertContext tests

--- reactapp/__tests__/components/contexts/LayoutAlertContext.test.js
+++ reactapp/__tests__/components/contexts/LayoutAlertContext.test.js
@@ -7,25 +7,25 @@ import LayoutAlertContextProvider, {
 } from "components/contexts/LayoutAlertContext";
 import Alert from "react-bootstrap/Alert";
 
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 const TestingComponent = () => {
-  const {
+  const [
     successMessage,
     setSuccessMessage,
     showSuccessMessage,
     setShowSuccessMessage,
-  } = useLayoutSuccessAlertContext();
-  const {
-    errorMessage,
-    setErrorMessage,
-    showErrorMessage,
-    setShowErrorMessage
-  } = useLayoutErrorAlertContext();
-  const {
+  ] = useLayoutSuccessAlertContext();
+  const [errorMessage, setErrorMessage, showErrorMessage, setShowErrorMessage] =
+    useLayoutErrorAlertContext();
+  const [
     warningMessage,
     setWarningMessage,
     showWarningMessage,
     setShowWarningMessage,
-  } = useLayoutWarningAlertContext();
+  ] = useLayoutWarningAlertContext();
 
   useEffect(() => {
     setSuccessMessage("success");
@@ -59,8 +59,6 @@ const TestingComponent = () => {
 };
 
 test("layout alert context", async () => {
-  jest.useFakeTimers();
-
   render(
     <LayoutAlertContextProvider>
       <TestingComponent />
@@ -71,13 +69,11 @@ test("layout alert context", async () => {
   expect(await screen.findByText("error")).toBeInTheDocument();
   expect(await screen.findByText("warning")).toBeInTheDocument();
 
-  act(() => {
-    jest.advanceTimersByTime(7000);
+  await act(async () => {
+    await sleep(6000);
   });
 
   expect(screen.queryByText("success")).not.toBeInTheDocument();
   expect(screen.queryByText("error")).not.toBeInTheDocument();
   expect(screen.queryByText("warning")).not.toBeInTheDocument();
-
-  jest.useRealTimers();
-});
+}, 7000);
