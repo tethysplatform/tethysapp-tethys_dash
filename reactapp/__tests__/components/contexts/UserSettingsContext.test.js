54c6597ad77fe3ab91ad547417244f3da8da99e6
Corey Krewson <ckrewson@aquaveo.com>
2024-12-05 14:44:33 -0600

    user settings context tests

---
+++ reactapp/__tests__/components/contexts/UserSettingsContext.test.js
@@ -0,0 +1,91 @@
+import { render, screen, waitFor, fireEvent } from "@testing-library/react";
+import { AppContext } from "components/contexts/AppContext";
+import RoutesContextProvider from "components/contexts/RoutesContext";
+import UserSettingsContextProvider, {
+  useUserSettingsContext,
+} from "components/contexts/UserSettingsContext";
+import appAPI from "services/api/app";
+
+const TestingComponent = () => {
+  const { userSettings, updateUserSettings } = useUserSettingsContext();
+
+  function updateSettings() {
+    updateUserSettings({ deselected_visualizations: [1, 2] });
+  }
+
+  return (
+    <>
+      <button
+        data-testid="user-settings-button"
+        onClick={updateSettings}
+      ></button>
+      <p data-testid="user-settings">{JSON.stringify(userSettings)}</p>
+    </>
+  );
+};
+
+test("user setting context update success", async () => {
+  appAPI.updateUserSettings = () => {
+    return Promise.resolve({
+      success: true,
+    });
+  };
+
+  render(
+    <AppContext.Provider value={"csrf"}>
+      <RoutesContextProvider>
+        <UserSettingsContextProvider>
+          <TestingComponent />
+        </UserSettingsContextProvider>
+      </RoutesContextProvider>
+    </AppContext.Provider>
+  );
+
+  await waitFor(async () => {
+    expect(await screen.findByTestId("user-settings")).toHaveTextContent(
+      JSON.stringify({ deselected_visualizations: [] })
+    );
+  });
+
+  const updateButton = await screen.findByRole("button");
+  fireEvent.click(updateButton);
+
+  await waitFor(async () => {
+    expect(await screen.findByTestId("user-settings")).toHaveTextContent(
+      JSON.stringify({ deselected_visualizations: [1, 2] })
+    );
+  });
+});
+
+test("user setting context update fail", async () => {
+  appAPI.updateUserSettings = () => {
+    return Promise.resolve({
+      success: false,
+    });
+  };
+
+  render(
+    <AppContext.Provider value={"csrf"}>
+      <RoutesContextProvider>
+        <UserSettingsContextProvider>
+          <TestingComponent />
+        </UserSettingsContextProvider>
+      </RoutesContextProvider>
+    </AppContext.Provider>
+  );
+
+  await waitFor(async () => {
+    expect(await screen.findByTestId("user-settings")).toHaveTextContent(
+      JSON.stringify({ deselected_visualizations: [] })
+    );
+  });
+
+  const updateButton = await screen.findByRole("button");
+  fireEvent.click(updateButton);
+
+  await waitFor(async () => {
+    expect(await screen.findByTestId("user-settings")).toHaveTextContent(
+      JSON.stringify({ deselected_visualizations: [] })
+    );
+  });
+});
