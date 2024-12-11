26e7a3d58b80a451f6d71c94c53c4f06aae41dfc
Corey Krewson <ckrewson@aquaveo.com>
2024-12-11 13:19:20 -0600

    added app info button and modal

--- reactapp/components/layout/Header.js
+++ reactapp/components/layout/Header.js
@@ -4,13 +4,14 @@ import Form from "react-bootstrap/Form";
 import Navbar from "react-bootstrap/Navbar";
 import PropTypes from "prop-types";
 import { useContext, useState } from "react";
-import { BsX, BsGear, BsList } from "react-icons/bs";
+import { BsX, BsGear, BsList, BsInfo } from "react-icons/bs";
 import { useLocation } from "react-router-dom";
 
 import TooltipButton from "components/buttons/TooltipButton";
 import { AppContext } from "components/contexts/Contexts";
 import DashboardSelector from "components/layout/DashboardSelector";
 import DashboardEditorCanvas from "components/modals/DashboardEditor";
+import AppInfoModal from "components/modals/AppInfo";
 import { LayoutContext } from "components/contexts/Contexts";
 import "components/buttons/HeaderButton.css";
 
@@ -22,6 +23,10 @@ const Header = ({ initialDashboard }) => {
   const { tethysApp, user } = useContext(AppContext);
   const location = useLocation();
   const [showEditCanvas, setShowEditCanvas] = useState(false);
+  const dontShowInfoOnStart = localStorage.getItem("dontShowInfoOnStart");
+  const [showInfoModal, setShowInfoModal] = useState(
+    dontShowInfoOnStart !== "true"
+  );
   const showNav = () => setShowEditCanvas(true);
   const { getLayoutContext } = useContext(LayoutContext);
   const { name } = getLayoutContext();
@@ -50,6 +55,14 @@ const Header = ({ initialDashboard }) => {
             <DashboardSelector initialDashboard={initialDashboard} />
           )}
           <Form inline="true">
+            <TooltipButton
+              onClick={() => setShowInfoModal(true)}
+              tooltipPlacement="bottom"
+              tooltipText="App Info"
+              aria-label={"appInfoButton"}
+            >
+              <BsInfo size="1.5rem" />
+            </TooltipButton>
             {user.isStaff && (
               <TooltipButton
                 href={tethysApp.settingsUrl}
@@ -75,6 +88,12 @@ const Header = ({ initialDashboard }) => {
         <DashboardEditorCanvas
           showCanvas={showEditCanvas}
           setShowCanvas={setShowEditCanvas}
+        />
+      )}
+      {showInfoModal && (
+        <AppInfoModal
+          showModal={showInfoModal}
+          setShowModal={setShowInfoModal}
         />
       )}
     </>
