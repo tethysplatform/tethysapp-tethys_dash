0602de2c74ff3132d02d49babe13ed3327737596
Corey Krewson <ckrewson@aquaveo.com>
2025-06-13 13:23:53 -0500

    removing feature tests

--- reactapp/__tests__/components/map/DrawInteractions.test.js
+++ reactapp/__tests__/components/map/DrawInteractions.test.js
@@ -4,6 +4,9 @@ import DrawInteractions from "components/map/DrawInteractions";
 import PropTypes from "prop-types";
 import { createRef } from "react";
 import VectorSource from "ol/source/Vector";
+import Feature from "ol/Feature";
+import Point from "ol/geom/Point";
+import { fromLonLat } from "ol/proj";
 import { Draw } from "ol/interaction";
 
 test("Draw Interactions click draw and then deselect", async () => {
@@ -99,4 +102,135 @@ test("Draw Interactions clear features", async () => {
   fireEvent.click(clearFeaturesButton);
 
   expect(sourceClear).toHaveBeenCalled();
+});
+
+test("Draw Interactions drawend", async () => {
+  const sourceRemoveFeature = jest.spyOn(
+    VectorSource.prototype,
+    "removeFeature"
+  );
+
+  const addedInteractions = [];
+  let interactionLayer;
+  const mockMap = {
+    addLayer: jest.fn((layer) => {
+      interactionLayer = layer;
+    }),
+    addInteraction: jest.fn((interaction) => {
+      addedInteractions.push(interaction); // Capture Draw
+    }),
+    removeInteraction: jest.fn(),
+  };
+
+  const mapDrawing = { options: ["Point"], limit: 1 };
+  const visualizationRef = { current: mockMap };
+  const drawing = { current: false };
+
+  render(
+    <DrawInteractions
+      mapDrawing={mapDrawing}
+      visualizationRef={visualizationRef}
+      drawing={drawing}
+    />
+  );
+
+  // Click the "Draw Point" button to trigger interaction setup
+  fireEvent.click(screen.getByTitle("Draw Point"));
+
+  // Wait for the interaction to be created and added
+  expect(addedInteractions.length).toBeGreaterThan(0);
+  const drawInteraction = addedInteractions[0];
+
+  let vectorSource = interactionLayer.getSource();
+
+  // add some features for tracking
+  const feature = new Feature({
+    geometry: new Point(fromLonLat([-123.1, 49.3])), // longitude, latitude
+    name: "My Point Feature",
+  });
+  vectorSource.addFeature(feature);
+
+  const feature2 = new Feature({
+    geometry: new Point(fromLonLat([-123.1, 49.3])), // longitude, latitude
+    name: "My Point Feature 2",
+  });
+  vectorSource.addFeature(feature2);
+
+  expect(vectorSource.getFeatures().length).toBe(2);
+
+  // Now we can safely dispatch the event
+  act(() => {
+    drawInteraction.dispatchEvent({
+      type: "drawend",
+    });
+  });
+
+  expect(sourceRemoveFeature).toHaveBeenCalledWith(feature);
+  expect(vectorSource.getFeatures().length).toBe(1);
+});
+
+test("Draw Interactions update limit and remove existing", async () => {
+  const sourceRemoveFeature = jest.spyOn(
+    VectorSource.prototype,
+    "removeFeature"
+  );
+
+  const addedInteractions = [];
+  let interactionLayer;
+  const mockMap = {
+    addLayer: jest.fn((layer) => {
+      interactionLayer = layer;
+    }),
+    addInteraction: jest.fn((interaction) => {
+      addedInteractions.push(interaction); // Capture Draw
+    }),
+    removeInteraction: jest.fn(),
+  };
+
+  const mapDrawing = { options: ["Point"], limit: 2 };
+  const visualizationRef = { current: mockMap };
+  const drawing = { current: false };
+
+  const { rerender } = render(
+    <DrawInteractions
+      mapDrawing={mapDrawing}
+      visualizationRef={visualizationRef}
+      drawing={drawing}
+    />
+  );
+
+  // Click the "Draw Point" button to trigger interaction setup
+  fireEvent.click(screen.getByTitle("Draw Point"));
+
+  // Wait for the interaction to be created and added
+  expect(addedInteractions.length).toBeGreaterThan(0);
+
+  let vectorSource = interactionLayer.getSource();
+
+  // add some features for tracking
+  const feature = new Feature({
+    geometry: new Point(fromLonLat([-123.1, 49.3])), // longitude, latitude
+    name: "My Point Feature",
+  });
+  vectorSource.addFeature(feature);
+
+  const feature2 = new Feature({
+    geometry: new Point(fromLonLat([-123.1, 49.3])), // longitude, latitude
+    name: "My Point Feature 2",
+  });
+  vectorSource.addFeature(feature2);
+
+  expect(vectorSource.getFeatures().length).toBe(2);
+
+  const newMapDrawing = { options: ["Point"], limit: 1 };
+  rerender(
+    <DrawInteractions
+      mapDrawing={newMapDrawing}
+      visualizationRef={visualizationRef}
+      drawing={drawing}
+    />
+  );
+
+  expect(sourceRemoveFeature).toHaveBeenCalledWith(feature);
+  expect(vectorSource.getFeatures().length).toBe(1);
 });
