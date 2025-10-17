da7681b66aa40c6c12f2c30da156b7b10054239f
Corey Krewson <ckrewson@aquaveo.com>
2025-10-17 16:39:50 -0500

    updated webpack and react code to handle prefixes with tethys

--- reactapp/services/utilities.js
+++ reactapp/services/utilities.js
@@ -2,6 +2,7 @@ const APP_ROOT_URL = process.env.TETHYS_APP_ROOT_URL;
 
 export function getTethysPortalHost() {
   let tethys_portal_host = process.env.TETHYS_PORTAL_HOST;
+  let tethys_prefix_url = process.env.TETHYS_PREFIX_URL.replace(/^\/|\/$/g, "");
 
   // If the .env property is not set, derive from current location
   if (!tethys_portal_host || !tethys_portal_host.length) {
@@ -9,10 +10,18 @@ export function getTethysPortalHost() {
     let url = new URL(currLocation);
     tethys_portal_host = url.origin;
   }
-  return tethys_portal_host;
+  // return tethys_portal_host;
+  return `${tethys_portal_host}/${tethys_prefix_url}`;
 }
 
 export const getPublicUrl = (uuid) => {
   return new URL(getTethysPortalHost() + APP_ROOT_URL + "dashboard/" + uuid)
     .href;
 };
+
+export function getTethysAppRoot() {
+  let tethys_app_root_url = process.env.TETHYS_APP_ROOT_URL;
+  let tethys_prefix_url = process.env.TETHYS_PREFIX_URL.replace(/^\/|\/$/g, "");
+  let fp = `/${tethys_prefix_url}/${tethys_app_root_url}`;
+  return fp.replace(/\/{2,}/g, "/");
+}
