36cdd668b335a09c380e2d0d1e94ea427abaad3f
Corey Krewson <ckrewson@aquaveo.com>
2026-01-26 15:06:08 -0600

    lint errors

--- reactapp/components/inputs/RuleEditor.js
+++ reactapp/components/inputs/RuleEditor.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef, memo, Fragment } from "react";
+import { useEffect, useState, useRef, memo } from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import NormalInput from "components/inputs/NormalInput";
